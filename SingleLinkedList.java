e280f242ca69c56c523cc5618a63112c72896d26
ankireddypraneetha623 <ankireddypraneetha623@gmail.com>
2025-08-19 01:35:57 -0700

    Add files via upload

---
+++ SingleLinkedList.java
@@ -0,0 +1,42 @@
+public class Singlelinklist {
+    static Node head;
+
+    static class Node {
+        int data;
+        Node next;
+
+        public Node(int data) {
+
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+        static void traverse() {
+            Node curr = head;
+            while (curr != null) {
+                System.out.print(curr.data + "<----->");
+
+                curr = curr.next;
+
+            }
+            System.out.println("null");
+        }
+
+
+        public static void main(String[] args) {
+            head = new Node(1);
+            Node second = new Node(2);
+            Node third = new Node(3);
+            Node fourth = new Node(4);
+            head.next = second;
+            second.next = third;
+            third.next = fourth;
+            traverse();
+            System.out.println("\n"+second);
+            System.out.println("\n"+third.data);
+           System.out.println("\n"+fourth.data);
+        }
+    }
+
+
